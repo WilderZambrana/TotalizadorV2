6d8c9eb6ee8ea41809b78819134aaf6501a67940
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-09-24 16:16:57 -0400

    Completando 2da Prueba

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ function precioTotal(cantidad, precio) {
   return cantidad * precio;
 }
 
-function impuesto(precioTotal,estado) {
-
+function impuesto(valor, estado) {
+  let valorImpuestos = 0;
+  switch (estado) {
+    case 'UT':
+      valorImpuestos = 0.0665;
+      break;
+    case 'NV':
+      valorImpuestos = 0.08;
+      break;
+    case 'TX':
+      valorImpuestos = 0.0625;
+      break;
+    case 'AL':
+      valorImpuestos = 0.04;
+      break;
+    case 'CA':
+      valorImpuestos = 0.0825;
+      break;
+    default:
+      alert("No existe");
+  }
+  return valor * valorImpuestos;
 }
 
 export { precioTotal, impuesto };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import precioTotal from "./App";
+import { precioTotal, impuesto } from "./App";
 
 const cantidad_input = document.querySelector("#cantidad-input");
 const precio_input = document.querySelector("#precio-input");
+const estado_input = document.querySelector("#estado-input");
 const form = document.querySelector("#totalizador-form");
 
 form.addEventListener("submit", (event) => {
@@ -9,10 +10,14 @@ form.addEventListener("submit", (event) => {
 
   const cantidad = Number.parseInt(cantidad_input.value);
   const precio = Number.parseInt(precio_input.value);
+  const estado = estado_input.value;
+
+  const precioInicial = precioTotal(cantidad, precio);
 
   document.getElementById('cantidad').innerHTML = cantidad;
   document.getElementById('precio').innerHTML = precio;
-  document.getElementById('totalCantxPrec').innerHTML = precioTotal(cantidad,precio);
+  document.getElementById('totalCantxPrec').innerHTML = precioInicial;
+  document.getElementById('impuesto').innerHTML = impuesto(precioInicial, estado);
 });
 
 
